eb9db5dc28db58d5d76684304b072dcf7d273cbd
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2017-12-17 22:45:11 -0800

    update path to assets

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/just-enough-fp/dist/'
   },
   plugins: [
     new webpack.DefinePlugin({
